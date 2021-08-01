db82efb02e597df1f1932d5b9a6ff543d284fda0
Josh Collinsworth <josh.collinsworth@aware3.com>
2021-08-01 16:25:47 -0500

    Beginning to put some things together
    Still trying to get SCSS working but NOT in the static folder. Doesn't seem like there's a lot of info on this.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +9,16 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		//Added for static adapter
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
-};
+}
 
 export default config;
