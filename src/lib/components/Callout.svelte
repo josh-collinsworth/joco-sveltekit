1da2fdf4a47360cab914f64d39cbd85f3a5671eb
Josh <josh@joshs-air.lan>
2021-08-14 14:34:02 -0500

    Minor changes from Air to recoup some lost work from MBP

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -7,9 +7,7 @@
 </template>
 
 
-<style type="text/scss">
-@import '../assets/css/scss/_vars';
-
+<style>
 .callout {
   margin: 3rem 1rem;
   font-style: italic;
@@ -20,48 +18,60 @@
   overflow: hidden;
   padding: 0 1rem 1rem 0;
   display: none;
+}
 
-  p {
-    margin: 0 1em;
-  }
+.callout p {
+  margin: 0 1em;
+}
 
-  p:last-of-type {
+.callout p:last-of-type {
     margin: 0;
+}
 
-    &:after {
-      content: '';
-      height: .25rem;
-      width: 100%;
-      margin-left: -100%;
-      background: var(--yellow);
-      bottom: 0;
-      position: absolute;
-    }
-  }
+.callout p:last-of-type:after {
+  content: '';
+  height: .25rem;
+  width: 100%;
+  margin-left: -100%;
+  background: var(--yellow);
+  bottom: 0;
+  position: absolute;
+}
+ 
 
-  @media(min-width: 768px) {
+@media(min-width: 768px) {
+  .callout {
     margin: 3rem -2rem 3rem 3rem;
     padding-right: 0;
     display: block;
     float: right;
     width: 50%;
   }
+}
 
-  @media(min-width: 1024px) {
+@media(min-width: 1024px) {
+  .callout {
     width: 20em;
     margin-right: calc(-3rem - 12vw);
   }
+}
 
-  @media(min-width: 1200px) {
+@media(min-width: 1200px) {
+  .callout {
     margin-right: calc(-2rem - 12vw);
   }
+}
 
-  @media(min-width: 1680px) {
+@media(min-width: 1680px) {
+  .callout {
     margin-right: -8rem;
   }
+}
 
-  @media(min-width: 1960px) {
+@media(min-width: 1960px) {
+  .callout {
     margin-right: -12rem;
   }
 }
+
 </style>
