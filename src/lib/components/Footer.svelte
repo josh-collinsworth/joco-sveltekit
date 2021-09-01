d1bcd7a5736b29014bfadfb00faa48469965fd42
Josh Collinsworth <josh.collinsworth@shopify.com>
2021-09-01 16:49:45 -0500

    Fix missing xml RSS feed link (well, comment it out anyway)

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,8 @@
   <footer class="footer">
     <div>
       <ul class="footer__links">
-        <li><a href="/rss.xml">RSS</a></li>
+        <!-- TODO: find an XML replacement? -->
+        <!-- <li><a href="/rss.xml">RSS</a></li> -->
         <li><a rel="external" href="github.com/josh-collinsworth">GitHub</a></li>
         <li><a rel="external" href="codepen.io/collinsworth">CodePen</a></li>
         <li><a rel="external" href="twitter.com/jjcollinsworth">Twitter</a></li>
