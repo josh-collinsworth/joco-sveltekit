37f9d183681a1003d75da227c87f9e1ab191fe5f
Josh Collinsworth <josh.collinsworth@shopify.com>
2021-09-24 17:06:15 -0500

    A very hacky (but working!) update to ToC

--- src/lib/components/posts/TableOfContents.svelte
+++ src/lib/components/posts/TableOfContents.svelte
@@ -4,10 +4,12 @@ import { end_hydrating } from 'svelte/internal';
 
   let headings = [];
 
-  const scrollToHeading = (level) => {
-    document.getElementById(`heading-${level}`).scrollIntoView({
-      behavior: 'smooth'
-    })
+  const scrollToHeading = (e) => {
+    if (e.target.href) {
+      const target = e.target.href.split('#').pop()
+      console.log(target)
+      document.getElementById(target).scrollIntoView({ behavior: 'smooth' })
+    }
   }
 
   const generatePrefix = (level: number): string => {
@@ -18,8 +20,6 @@ import { end_hydrating } from 'svelte/internal';
     const iterator = new Array(level).fill('')
     let prefix = '    '
 
-    console.log(iterator)
-
     iterator.forEach((_, i) => {
       if (i + 1 == iterator.length) {
         prefix += '•'
@@ -28,10 +28,10 @@ import { end_hydrating } from 'svelte/internal';
       }
     })
 
-    console.log(prefix)
-
     return prefix
   }
+
+  let output = ''
 
   onMount(() => {
     let allHeadings = document.querySelectorAll('article .toc ~ :is(h2, h3, h4, h5, h6)');
@@ -40,50 +40,95 @@ import { end_hydrating } from 'svelte/internal';
       const level = parseInt(h.tagName.slice(1)) - 2
       
       return {
-        name: h.innerText,
+        title: h.innerText,
         level,
         prefix: generatePrefix(level),
         number: i
       }
     })
+
+    headings.forEach((h, i) => {
+      const link = `<a href="#heading-${i}">${h.title}</a>`
+      if (i === 0) {
+        output += `<li>${link}`
+      } else if (headings[i - 1].level === h.level) {
+        output += `</li><li>${link}`
+      } else if (headings[i - 1].level < h.level) {
+        output += `<ul><li>${link}`
+      } else if (headings[i - 1].level > h.level) {
+        console.log(h.title)
+        const subtraction =
+          i + 1 === headings.length
+            ? h.level + 1
+            : headings[i - 1].level - h.level 
+        console.log(`going up ${subtraction} levels`)
+        for (let n = 0; n <= subtraction; n++) {
+          output += '</li></ul>'
+        }
+        output += `</li><li>${link}`
+      }
+    }) 
   })
 </script>
 
 <aside class="toc">
   <h2>Table of contents:</h2>
 
-  {#each headings as heading}
+  <ul class="toc-list" on:click|preventDefault={scrollToHeading}>
+    {@html output}
+  </ul>
+  <!-- {#each headings as heading}
   <div>
     <a href="#heading-{heading.number}" on:click|preventDefault={scrollToHeading(heading.number)}>
       <span class="h-{heading.level}">
         {heading.prefix}
-        {heading.name}
+        {heading.title}
       </span>
     </a>
   </div>
-  {/each}
+  {/each} -->
 </aside>
 
 
-<style lang="scss">
+<style lang="scss" global>
   .toc {
     background: rgba(var(--lightBlueRGB), 0.2);
     padding: 1.5rem;
     line-height: 1.6;
-    margin: 1rem 0;
     border: 1px solid;
-    white-space: pre;
+    margin: 0 0 2rem;
+    width: max-content;
+    
+    .toc-list {
+      list-style-type: decimal;
+      font-size: 0.9rem;
+      margin: 0;
+      
+      ul {
+        list-style-type: lower-alpha;
+        margin: 0.5em 0 0;
+
+        ul {
+          list-style-type: decimal;
+        }
+      }
+      
+      li {
+        list-style-type: inherit;
+        margin: 0 0 0.5em;
+
+        &::before {
+          content: unset;
+        }
+      }
+    }
+
 
     h2 {
       font-weight: bold;
       font-family: var(--body-font);
       font-size: 1.3rem;  
-    }
-  
-    .h-0 {
-      font-weight: bold;
-      display: inline-block;
-      margin-top: 0.5rem;
+      margin-bottom: 2rem;
     }
   }
 </style>
