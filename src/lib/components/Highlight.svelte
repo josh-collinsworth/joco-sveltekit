9dc3a702d73085cd328602e9d2c09b48323e95f3
Josh Collinsworth <josh.collinsworth@shopify.com>
2021-10-05 17:16:35 -0500

    Fix deploy error

---
+++ src/lib/components/Highlight.svelte
@@ -0,0 +1,14 @@
+<div class="highlight">
+  <slot />
+</div>
+
+<style lang="scss">
+  .highlight {
+    font-size: 1.3em;
+    line-height: 1.5;
+    margin: 0 0 2rem;
+    font-weight: normal;
+    font-style: normal;
+    font-family: var(--accent-font);
+  }
+</style>
