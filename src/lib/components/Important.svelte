8ae9187691df35d11896cca6cbf10be855952dbb
Josh Collinsworth <josh.collinsworth@shopify.com>
2021-12-20 10:42:32 -0600

    Trying out a new Important component; fix heading missing links and move rehype section; add RSS section

---
+++ src/lib/components/Important.svelte
@@ -0,0 +1,44 @@
+<div class="warning">
+  <div class="inner">
+    <slot />
+  </div>
+</div>
+
+
+<style lang="scss">
+  .warning {
+    width: calc(100% + (var(--rhythm) * 2));
+    margin: calc(var(--rhythm) * 2) 0;
+    margin-left: calc(var(--rhythm) * -1);
+    // margin: 2.5rem 0;
+    background: linear-gradient(to bottom right, var(--yellow), var(--orange));
+    // background: var(--yellow);
+    position: relative;
+    padding: 2px;
+    font-size: 0.85rem;
+    font-style: italic;
+    
+    .inner {
+      padding: calc(var(--rhythm) * 1);
+      background: var(--paper);
+    }
+
+    &::before {
+      content: '!important';
+      font-family: 'MonoLisa', monospace;
+      font-style: normal;
+      background: var(--paper);
+      position: absolute;
+      top: -0.9em;
+      left: 0.9em;
+      line-height: 1;
+      display: block;
+      padding: .5em 1em;
+      // font-family: var(--heading-font);
+      text-align: center;
+      // text-transform: uppercase;
+      // font-weight: bold;
+      font-size: 0.7rem;
+    }
+  }
+</style>
