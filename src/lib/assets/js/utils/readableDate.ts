1df97e53f53944da5b4b99b401e40e3361d97760
Jeremy Bunting <187095+qbunt@users.noreply.github.com>
2022-09-20 22:58:48 -0400

    Improved date formatting
    Was curious about how you did something on your site, and found `readableDate` and thought it could use some love. Hope you don't mind, your post on the sveltekit changes are so incredibly helpful to get up to speed quickly. Cheers man!

--- src/lib/assets/js/utils/readableDate.ts
+++ src/lib/assets/js/utils/readableDate.ts
@@ -1,29 +1,10 @@
-const months: string[] = [
-  null, // January should be 1, not 0, etc.
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
-
 const readableDate = (date: string): string => {
-  if (!date) return date
-
-  const splitDate = date.split('-')
-
-  const month: string = months[parseInt(splitDate[1])]
-  const day: number = parseInt(splitDate[2])
-  const year: string = splitDate[0]
-
-  return `${month} ${day}, ${year}`
+  return !date ? date : new Date(`${date}T00:00:00`).toLocaleString('en-US', {
+    'timeZone': 'UTC',
+    'month': 'long',
+    'year': 'numeric',
+    'day': 'numeric'
+  })
 }
 
 export default readableDate
