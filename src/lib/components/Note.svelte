484ad6ee4995ace0820099d37e7975b71462b6ac
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2023-03-12 10:42:16 -0500

    Fix note component to render properly outside Firefox

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -1,7 +1,8 @@
 <div class="note">
 	<div class="note__callout">
-		Note
-		<span class="note__callout-arrow" />
+		<div class="note__callout-text">
+			Note <span class="note__callout-arrow" />
+		</div>
 	</div>
 	<div class="note__content">
 		<slot />
@@ -18,7 +19,6 @@
 	
 	.note__callout {
 		background: var(--yellow);
-		writing-mode: sideways-lr;
 		text-align: center;
 		line-height: 1.5;
 		text-transform: uppercase;
@@ -26,6 +26,16 @@
 		font-weight: bold;
 		font-style: normal;
 		font-family: var(--headingFont);
+		width: 1.5rem;
+		display: grid;
+		place-content: center;
+	}
+
+	.note__callout-text {
+		transform: rotate(-90deg);
+		white-space: pre;
+		display: flex;
+		align-items: center;
 	}
 
 	.note__callout-arrow {
@@ -33,7 +43,7 @@
 		width: 0.7em;
 		height: 0.75em;
 		border-top: 1px solid;
-		border-left: 1px solid;
+		border-right: 1px solid;
 		position: relative;
 
 		&::after {
@@ -41,11 +51,11 @@
 			width: 0.5em;
 			height: 0.5em;
 			border-right: 1px solid;
-			border-top: 1px solid;
+			border-bottom: 1px solid;
 			position: absolute;
 			transform: rotate(45deg);
-			right: 0;
-			top: calc(-0.2em - 1px);
+			top: 2px;
+			right: calc(-0.2em - 1px);
 		}
 	}
 	
