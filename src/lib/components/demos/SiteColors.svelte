7ca875ee0897a9d0b6e82bd0d62a7d58b26c7685
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2023-10-11 09:45:26 -0500

    Fix project autosave config; /uses colors update

--- src/lib/components/demos/SiteColors.svelte
+++ src/lib/components/demos/SiteColors.svelte
@@ -1,47 +1,80 @@
 <script lang="ts">
-	const colors: string[] = [
-		'#ffd100',
-		'#34657f',
-		'#7ba7bc',
-		'#ff6a13',
-		'#e4002b',
+	const colors: string[] = ['#ffd100', '#34657f', '#7ba7bc', '#ff6a13', '#e6002a']
+
+	const grays: string[] = [
+		'#fcfcfc',
 		'#a7a8aa',
-		'#888b8d',
+		'#878a8c',
 		'#53565a',
-		'#341F23',
-		'#101820',
+		'#424548',
+		'#331f22',
+		'#101820'
 	]
+
+	const getOutlineColor = (color) => {
+		if (color === '#101820') return '#fcfcfc'
+		if (color === '#fcfcfc') return '#101820'
+		return 'transparent'
+	}
 </script>
 
-<ul class="brand-colors no-bullets">
-	{#each colors as color}
-		<li class="color">
-			<div class="swatch" style="background-color: {color}">
-			</div>
-			<span>{color}</span>
-		</li>
-	{/each}
-</ul>
-
+<div class="colors-container">
+	<ul class="brand-colors no-bullets">
+		{#each colors as color}
+			<li class="color">
+				<div
+					class="swatch {color}"
+					style="background-color: {color}; outline: 1px solid {getOutlineColor(color)}"
+				/>
+				<span>{color}</span>
+			</li>
+		{/each}
+	</ul>
+	<ul class="brand-colors no-bullets">
+		{#each grays as color}
+			<li class="color">
+				<div
+					class="swatch {color}"
+					style="background-color: {color}; outline: 1px solid {getOutlineColor(color)}"
+				/>
+				<div>{color}</div>
+			</li>
+		{/each}
+	</ul>
+</div>
 
 <style lang="scss">
+	.colors-container {
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: var(--quarterNote);
+		align-items: start;
+
+		@media (min-width: vars.$xxs) {
+			grid-template-columns: repeat(auto-fill, minmax(8rem, 1fr));
+		}
+	}
+
 	.brand-colors {
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(8.5rem, 1fr));
+		grid-template-columns: 1fr;
 		padding: 0;
-		
+		gap: var(--eighthNote);
+
 		.color {
 			list-style-type: none;
 			display: flex;
+			flex-wrap: wrap;
 			align-items: center;
-			font-size: .9rem;
+			font-size: 0.9rem;
 			font-family: var(--codeFont);
 			text-shadow: 0 0 1px var(--trueWhite);
-			
+			margin: 0;
+
 			.swatch {
 				width: 3.5em;
 				height: 3.5em;
-				margin-right: .5rem;
+				margin-right: 0.5rem;
 			}
 		}
 	}
