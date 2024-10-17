69978821e88cd2cc3485685fe1cef733c1c23359
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2024-10-17 16:35:52 -0500

    Add early return to sluggify fn

--- src/lib/assets/js/utils/sluggify.ts
+++ src/lib/assets/js/utils/sluggify.ts
@@ -1,8 +1,9 @@
 const sluggify = (str: string): string => {
+	if (!str) return ''
 	return str
-	.toLowerCase()
-	.replace(/\s/g, '_')
-	.replace(/[^A-z0-9_-]/g, '')
+		.toLowerCase()
+		.replace(/\s/g, '_')
+		.replace(/[^A-z0-9_-]/g, '')
 }
 
 export default sluggify
