a9355688fc377e5ce0f05b7b01aa82856a9ddae7
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2024-10-29 10:12:38 -0500

    Post updates; shuffle some CSS around

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -1,18 +1,3 @@
 <p class="callout">
 	<slot />
 </p>
-
-<style lang="scss">
-	.callout {
-		margin: var(--dottedQuarterNote) 0;
-		font-size: 1.3rem;
-		line-height: 1.4;
-
-		@media (min-width: vars.$lg) {
-			margin: 0 0 var(--quarterNote);
-			font-size: inherit;
-			line-height: inherit;
-			font-family: inherit;
-		}
-	}
-</style>
