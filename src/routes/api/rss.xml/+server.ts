c2d8b41e34d004073371308b1a2c62101d75422e
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2026-02-20 10:56:03 -0600

    Optimize RSS feed

--- src/routes/api/rss.xml/+server.ts
+++ src/routes/api/rss.xml/+server.ts
@@ -22,20 +22,29 @@ export const GET = async () => {
 	)
 }
 
+const renderCategories = (categories: Post['categories']): string => {
+	if (!categories) return ''
+	const list = Array.isArray(categories) ? categories : [categories]
+	return list.map((cat) => `<category>${cat}</category>`).join('\n')
+}
+
 const render = (posts: Post[]): string => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <title>Josh Collinsworth</title>
-<description>Josh Collinsworth</description>
+<description>Josh Collinsworth's blog</description>
+<language>en</language>
 <link>https://joshcollinsworth.com</link>
 <atom:link href="https://joshcollinsworth.com/api/rss.xml" rel="self" type="application/rss+xml"/>
+<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
 ${posts
 	.map(
 		(post) => `<item>
 <guid isPermaLink="true">https://joshcollinsworth.com/blog/${post.slug}</guid>
-<title>${post.title}</title>
+<title><![CDATA[${post.title}]]></title>
 <link>https://joshcollinsworth.com/blog/${post.slug}</link>
-<description>${post.excerpt}</description>
+<description><![CDATA[${post.excerpt}]]></description>
+${renderCategories(post.categories)}
 <pubDate>${new Date(post.date).toUTCString()}</pubDate>
 </item>`
 	)
